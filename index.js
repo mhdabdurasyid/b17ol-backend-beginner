c898dbd415c15a663ac4095795f313f4ebba5018
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-09-08 20:34:56 +0700

    add method GET to path '/home'

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require('express');
 
 const app = express();
+
+// using port 8080 to listening
+app.listen(8080, () => {
+    console.log('App listening on port 8080');
+});
+
+// Tugas no 1
+app.get('/home', (request, response) => {
+    response.send('Welcome to my first backend!');
+});
