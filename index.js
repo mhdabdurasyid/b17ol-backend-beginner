b486aa4588d24b8fc392f21f7443af62767da0e7
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-09-08 22:04:00 +0700

    add POST method to path '/data'

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+//configure express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // using port 8080 to listening
 app.listen(8080, () => {
@@ -11,3 +17,9 @@ app.listen(8080, () => {
 app.get('/home', (request, response) => {
     response.send('Welcome to my first backend!');
 });
+
+// Tugas no 2
+app.post('/data', (request, response) => {
+    console.log(request.body);
+    response.sendStatus(200);
+})
